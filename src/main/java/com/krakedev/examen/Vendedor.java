ef49e8e821eb7970a256c470688c6350f66fedb5
angel-hub31 <moralesmales1@hotmail.com>
2026-05-09 20:28:54 -0500

    se crea el metodo toString

--- src/main/java/com/krakedev/examen/Vendedor.java
+++ src/main/java/com/krakedev/examen/Vendedor.java
@@ -5,46 +5,54 @@ public class Vendedor {
 	private int numeroVentas;
 	private double sueldoFijo;
 	private double comisionPorVenta;
-	
-	
-	 public Vendedor(String cedula) {
-	        this.cedula = cedula;
-	 }
-	 
-	 
+
+	public Vendedor(String cedula) {
+		this.cedula = cedula;
+	}
+
 	public String getCedula() {
 		return cedula;
 	}
-	
+
 	// metodo calcularSueldo
-	 public double calcularSueldo() {
-	        return sueldoFijo;
-	    }
-	
-	
+	public double calcularSueldo() {
+		return sueldoFijo;
+	}
+
+	// metodo toString
+
 	public void setCedula(String cedula) {
 		this.cedula = cedula;
 	}
+
+	@Override
+	public String toString() {
+		return "Vendedor [cedula=" + cedula + ", numeroVentas=" + numeroVentas + ", sueldoFijo=" + sueldoFijo
+				+ ", comisionPorVenta=" + comisionPorVenta + "]";
+	}
+
 	public int getNumeroVentas() {
 		return numeroVentas;
 	}
+
 	public void setNumeroVentas(int numeroVentas) {
 		this.numeroVentas = numeroVentas;
 	}
+
 	public double getSueldoFijo() {
 		return sueldoFijo;
 	}
+
 	public void setSueldoFijo(double sueldoFijo) {
 		this.sueldoFijo = sueldoFijo;
 	}
+
 	public double getComisionPorVenta() {
 		return comisionPorVenta;
 	}
+
 	public void setComisionPorVenta(double comisionPorVenta) {
 		this.comisionPorVenta = comisionPorVenta;
 	}
-	
-	
-	
 
 }
