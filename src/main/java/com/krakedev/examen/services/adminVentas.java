4a4419c405c19a19eb9eb26c6804a212bede0898
angel-hub31 <moralesmales1@hotmail.com>
2026-05-09 21:36:12 -0500

    se realiza un video explicando el error provocado de nullPointerExepcion

--- src/main/java/com/krakedev/examen/services/adminVentas.java
+++ src/main/java/com/krakedev/examen/services/adminVentas.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import com.krakedev.examen.Vendedor;
 
 public class adminVentas {
-	private ArrayList<Vendedor> vendedores = new ArrayList<>();
+	private ArrayList<Vendedor> vendedores=new ArrayList<>();
 	
 	public void agregar(Vendedor vendedor) {
         vendedores.add(vendedor);
