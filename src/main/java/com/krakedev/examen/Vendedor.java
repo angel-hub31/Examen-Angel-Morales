f3be7b84fce5ecb0166905576ec5c94cbd81cdaa
angel-hub31 <moralesmales1@hotmail.com>
2026-05-09 20:20:04 -0500

    se crea la clase Vendedor con sus atributos privados

---
+++ src/main/java/com/krakedev/examen/Vendedor.java
@@ -0,0 +1,11 @@
+package com.krakedev.examen;
+
+public class Vendedor {
+	private String cedula;
+	private int numeroVentas;
+	private double sueldoFijo;
+	private double comisionPorVenta;
+	
+	
+
+}
