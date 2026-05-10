078337fb3206db801528368113e2f3bd86478bc9
angel-hub31 <moralesmales1@hotmail.com>
2026-05-09 22:24:23 -0500

    se corrige errores

--- src/main/java/com/krakedev/examen/controller/VendedorController.java
+++ src/main/java/com/krakedev/examen/controller/VendedorController.java
@@ -1,11 +1,6 @@
 package com.krakedev.examen.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.krakedev.examen.Vendedor;
 import com.krakedev.examen.VendedorComision;
@@ -13,35 +8,53 @@ import com.krakedev.examen.VendedorMixto;
 import com.krakedev.examen.services.adminVentas;
 
 @RestController
-	@RequestMapping("/vendedores")
-	public class VendedorController {
-	    private static adminVentas admin = new adminVentas();
+@RequestMapping("/vendedores")
+public class VendedorController {
 
-	    @PostMapping("/agregar")
-	    public void agregarVendedor(@RequestBody Vendedor vendedor) {
-	        Vendedor nuevoVendedor;
-	        
-	        if (vendedor.getTipo().equals("V")) {
-	            nuevoVendedor = new Vendedor(vendedor.getCedula(), "V");
-	        } else if (vendedor.getTipo().equals("C")) {
-	            nuevoVendedor = new VendedorComision(vendedor.getCedula());
-	        } else if (vendedor.getTipo().equals("M")) {
-	            nuevoVendedor = new VendedorMixto(vendedor.getCedula());
-	        } else {
-	            return; 
-	        }
+    private static adminVentas admin = new adminVentas();
 
-	        nuevoVendedor.setSueldoFijo(vendedor.getSueldoFijo());
-	        nuevoVendedor.setComisionPorVenta(vendedor.getComisionPorVenta());
-	        nuevoVendedor.setNumeroVentas(vendedor.getNumeroVentas());
+    @PostMapping("/agregar")
+    public void agregarVendedor(@RequestBody Vendedor vendedor) {
 
-	        admin.agregar(nuevoVendedor);
-	    }
+        Vendedor nuevoVendedor = null;
 
-	    @GetMapping("/calcular/{cedula}")
-	    public Double calcularSueldoVendedor(@PathVariable String cedula) {
-	        return admin.calcularSueldo(cedula);
-	    }
-	}
+        if (vendedor.getTipo().equals("V")) {
 
+            nuevoVendedor = new Vendedor(
+                    vendedor.getCedula(),
+                    "V");
 
+        } else if (vendedor.getTipo().equals("C")) {
+
+            nuevoVendedor = new VendedorComision(
+                    vendedor.getCedula());
+
+        } else if (vendedor.getTipo().equals("M")) {
+
+            nuevoVendedor = new VendedorMixto(
+                    vendedor.getCedula());
+
+        } else {
+            return;
+        }
+
+        // copiar datos del JSON
+        nuevoVendedor.setSueldoFijo(
+                vendedor.getSueldoFijo());
+
+        nuevoVendedor.setNumeroVentas(
+                vendedor.getNumeroVentas());
+
+        nuevoVendedor.setComisionPorVenta(
+                vendedor.getComisionPorVenta());
+
+        admin.agregar(nuevoVendedor);
+    }
+
+    @GetMapping("/calcular/{cedula}")
+    public Double calcularSueldoVendedor(
+            @PathVariable String cedula) {
+
+        return admin.calcularSueldo(cedula);
+    }
+}
