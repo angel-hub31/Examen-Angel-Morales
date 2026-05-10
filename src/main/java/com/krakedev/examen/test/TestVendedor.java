f566746fd47543dd25336508018058fa18f3f783
angel-hub31 <moralesmales1@hotmail.com>
2026-05-09 20:32:52 -0500

    se crea la clase TestVendedor

---
+++ src/main/java/com/krakedev/examen/test/TestVendedor.java
@@ -0,0 +1,17 @@
+package com.krakedev.examen.test;
+
+import com.krakedev.examen.Vendedor;
+
+public class TestVendedor {
+
+	public static void main(String[] args) {
+		Vendedor vendedorUno = new Vendedor("1725896341");
+		
+		vendedorUno.setSueldoFijo(450.0);
+		
+		System.out.println("Datos del Vendedor:");
+        System.out.println(vendedorUno);
+        System.out.println("Sueldo calculado: " + vendedorUno.calcularSueldo());
+	}
+
+}
