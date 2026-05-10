9537af357e58e6e8957c08aa8fe311e482211ed0
angel-hub31 <moralesmales1@hotmail.com>
2026-05-09 20:24:05 -0500

    se crea el constructor y los metodos getters y setters

--- src/main/java/com/krakedev/examen/Vendedor.java
+++ src/main/java/com/krakedev/examen/Vendedor.java
@@ -7,6 +7,37 @@ public class Vendedor {
 	private double comisionPorVenta;
 	
 	
+	 public Vendedor(String cedula) {
+	        this.cedula = cedula;
+	 }
+	 
+	 
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public int getNumeroVentas() {
+		return numeroVentas;
+	}
+	public void setNumeroVentas(int numeroVentas) {
+		this.numeroVentas = numeroVentas;
+	}
+	public double getSueldoFijo() {
+		return sueldoFijo;
+	}
+	public void setSueldoFijo(double sueldoFijo) {
+		this.sueldoFijo = sueldoFijo;
+	}
+	public double getComisionPorVenta() {
+		return comisionPorVenta;
+	}
+	public void setComisionPorVenta(double comisionPorVenta) {
+		this.comisionPorVenta = comisionPorVenta;
+	}
+	
+	
 	
 
 }
