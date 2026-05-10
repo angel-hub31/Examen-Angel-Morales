9f481d8fdf50ef48c362302c4ca6c9fcc7b3e894
angel-hub31 <moralesmales1@hotmail.com>
2026-05-09 20:38:04 -0500

    se crea la prueba unitaria para validar el metodo calcularSueldo

---
+++ src/test/java/com/krakedev/examen/VendedorJUnitTest.java
@@ -0,0 +1,20 @@
+package com.krakedev.examen;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class VendedorJUnitTest {
+	
+	@Test
+    public void testCalcularSueldoVendedor() {
+        Vendedor vendedor = new Vendedor("1725896341");
+        double sueldoEsperado = 500.0;
+        
+        vendedor.setSueldoFijo(sueldoEsperado);
+        double sueldoObtenido = vendedor.calcularSueldo();
+        
+        assertEquals(sueldoEsperado, sueldoObtenido);
+    }
+
+}
